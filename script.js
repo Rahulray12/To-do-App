f8f065355a8d4489a2dc3df5dcf262214ad4f4db
Rahul Ray <132256030+Rahulray12@users.noreply.github.com>
2024-05-13 22:31:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,38 @@ const InputBox = document.getElementById("input-box");
 const ListContainer = document.getElementById("list-container");
 
 function addTask(){
-    if(InputBox === ""){
-        alert("You must Write sometning before adding!!!");
+    if(InputBox.value === ""){
+        alert("You must write something before adding!!!");
     }
     else{
         let li = document.createElement("li");
-        li.innerHTML = InputBox.Value;
+        li.innerHTML = InputBox.value; 
         ListContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-    InputBox.Value= "";
+    InputBox.value = ""; 
+    savedata();
 }
 
-addTask();
+ListContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false)
+
+function savedata() {
+    localStorage.setItem("data",ListContainer.innerHTML);
+}
+
+function showTask(){
+    ListContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
